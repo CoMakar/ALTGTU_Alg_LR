631913a46cd823b6cace0fdd6ddfbffdbf77a550
Makar <krikazon@mail.ru>
2023-10-25 16:53:46 +0700

    file structure revised

--- main.py
+++ main.py
@@ -1,49 +1,66 @@
-from importlib import import_module
 import traceback as tb
+import itertools as ittls
+from importlib import import_module
 from pathlib import Path
 
 
+restricted_dirs = {"lib", "res", ".vscode", ".git"}
+cwd = Path.cwd()
+dirs = [
+    f for f in cwd.iterdir() if f.is_dir() and f.name not in restricted_dirs
+]
+dir_files = {
+    d.name:
+    [
+        "{d}.{f}".format(d=d.name, f=f.name.rstrip(".py"))
+        for f in d.glob("*.py")
+        if f.name != "__init__.py"
+    ]
+    for d in dirs
+}
+files = list(ittls.chain.from_iterable(dir_files.values()))
+
+
 print("[- Press Ctrl-C to exit -]")
-
-lr_files = [f.name.rstrip(".py") for f in Path.cwd().rglob("LR_*_*.py")]
-
 print("Files:")
-for file in lr_files:
-    print(" - {}".format(file))
+for dir in dir_files:
+    print("  {}".format(dir))
+    for file in dir_files[dir]:
+        print("   └─ {}".format(file))
 print()
 
 
 try:
     while True:
-        print("[?] Run LR_N_V:")
-        lr_n = input("    N: ")
-        var = input("    V: ")
+        print("[?] Run:")
+        to_run = input(">>> ")
         
-        lr_name = "LR_{lr}_{var}".format(lr=lr_n, var=var)
-        lr_path = "LR_{lr}.{lr_name}".format(lr=lr_n, lr_name=lr_name)
+        if to_run not in files:
+            print(
+                "[-] Not found: {tr}".format(
+                    tr=to_run
+                )
+            )
+            continue
 
         try:
-            if lr_name not in lr_files:
-                print("[-] Not found: LR_{lr}.LR_{lr}_{var}".format(lr=lr_n, var=var))
-                continue
-            
-            lr_module = import_module(lr_path)
-            
+            run_module = import_module(to_run)
+
             print()
-            print(" ~  {}".format(lr_name))
-            print("-"*80)
-            
-            lr_module.main()
-            
-            print("-"*80)
-            print("[+] LR finished")
+            print(" ~  {}".format(to_run))
+            print("-" * 80)
+
+            run_module.main()
+
+            print("-" * 80)
+            print("[+] finished")
             print()
-            
+
         except Exception as e:
             print("[!] Error:")
-            print("~"*80)
+            print("~" * 80)
             tb.print_exc()
-            print("~"*80)
+            print("~" * 80)
 
 except KeyboardInterrupt:
     print()
